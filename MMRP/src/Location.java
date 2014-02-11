a77919be5f8e2968a53ba6c032a11ae5f63a074c
cjs9135@sru.edu <cjs9135@sru.edu>
2014-02-11 09:34:03 -0500

    Added Location Class-Chris

---
+++ MMRP/src/Location.java
@@ -0,0 +1,16 @@
+
+public class Location extends BaseClass {
+
+	@Override
+	void Update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	void Delete() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
